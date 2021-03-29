f00213eb58fc8bfb24d3f73ce5e9b4cbbce8baa9
brummer10 <brummer-@web.de>
2021-03-29 15:09:47 +0200

    initial commit

---
+++ XKeyboard/XKeyboard.c
@@ -0,0 +1,276 @@
+
+#define CONTROLS 5
+
+#define GUI_ELEMENTS 0
+
+#define TAB_ELEMENTS 0
+
+
+#define PLUGIN_UI_URI "https://github.com/brummer10/XKeyboard_ui"
+
+#define OBJ_BUF_SIZE 1024
+
+#include "lv2_plugin.h"
+
+
+#include "xkeyboard.c"
+
+void set_costum_theme(Xputty *main) {
+    main->color_scheme->normal = (Colors) {
+         /* cairo    / r  / g  / b  / a  /  */
+        .fg =       { 0.850, 0.850, 0.850, 1.000},
+        .bg =       { 0.100, 0.100, 0.100, 1.000},
+        .base =     { 0.000, 0.000, 0.000, 1.000},
+        .text =     { 0.900, 0.900, 0.900, 1.000},
+        .shadow =   { 0.000, 0.000, 0.000, 0.200},
+        .frame =    { 0.000, 0.000, 0.000, 1.000},
+        .light =    { 0.100, 0.100, 0.100, 1.000}
+    };
+
+    main->color_scheme->prelight = (Colors) {
+         /* cairo    / r  / g  / b  / a  /  */
+        .fg =       { 1.000, 0.000, 1.000, 1.000},
+        .bg =       { 0.250, 0.250, 0.250, 1.000},
+        .base =     { 0.300, 0.300, 0.300, 1.000},
+        .text =     { 1.000, 1.000, 1.000, 1.000},
+        .shadow =   { 0.100, 0.100, 0.100, 0.400},
+        .frame =    { 0.300, 0.300, 0.300, 1.000},
+        .light =    { 0.300, 0.300, 0.300, 1.000}
+    };
+
+    main->color_scheme->selected = (Colors) {
+         /* cairo    / r  / g  / b  / a  /  */
+        .fg =       { 0.900, 0.900, 0.900, 1.000},
+        .bg =       { 0.200, 0.200, 0.200, 1.000},
+        .base =     { 0.500, 0.180, 0.180, 1.000},
+        .text =     { 1.000, 1.000, 1.000, 1.000},
+        .shadow =   { 0.800, 0.180, 0.180, 0.200},
+        .frame =    { 0.500, 0.180, 0.180, 1.000},
+        .light =    { 0.500, 0.180, 0.180, 1.000}
+    };
+
+    main->color_scheme->active = (Colors) {
+         /* cairo    / r  / g  / b  / a  /  */
+        .fg =       { 0.000, 1.000, 1.000, 1.000},
+        .bg =       { 0.000, 0.000, 0.000, 1.000},
+        .base =     { 0.180, 0.380, 0.380, 1.000},
+        .text =     { 0.750, 0.750, 0.750, 1.000},
+        .shadow =   { 0.180, 0.380, 0.380, 0.500},
+        .frame =    { 0.180, 0.380, 0.380, 1.000},
+        .light =    { 0.180, 0.380, 0.380, 1.000}
+    };
+
+    main->color_scheme->insensitive = (Colors) {
+         /* cairo    / r  / g  / b  / a  /  */
+        .fg =       { 0.850, 0.850, 0.850, 0.500},
+        .bg =       { 0.100, 0.100, 0.100, 0.500},
+        .base =     { 0.000, 0.000, 0.000, 0.500},
+        .text =     { 0.900, 0.900, 0.900, 0.500},
+        .shadow =   { 0.000, 0.000, 0.000, 0.100},
+        .frame =    { 0.000, 0.000, 0.000, 0.500},
+        .light =    { 0.100, 0.100, 0.100, 0.500}
+    };
+}
+
+#include "lv2_plugin.cc"
+
+void get_note(Widget_t *w, const int *key, const bool on_off) {
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    uint8_t obj_buf[OBJ_BUF_SIZE];
+    lv2_atom_forge_set_buffer(&ui->forge, obj_buf, OBJ_BUF_SIZE);
+    LV2_Atom_Forge_Frame frame;
+    LV2_Atom* msg = (LV2_Atom*)lv2_atom_forge_object(&ui->forge, &frame, 0, ui->patch_Set);
+    lv2_atom_forge_key(&ui->forge, ui->patch_property);
+    lv2_atom_forge_urid(&ui->forge, on_off? ui->note_on : ui->note_off);
+    lv2_atom_forge_key(&ui->forge, ui->patch_value);    
+    lv2_atom_forge_int(&ui->forge, (int)(*key));
+    lv2_atom_forge_pop(&ui->forge, &frame);
+    ui->write_function(ui->controller, 0, lv2_atom_total_size(msg),
+                       ui->atom_eventTransfer, msg);
+}
+
+void send_vec(Widget_t *w, const int *key, const bool on_off) {
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    uint8_t obj_buf[OBJ_BUF_SIZE];
+    int vec[4];
+    vec[0] = (*key);
+    vec[1] = (int)adj_get_value(ui->widget[2]->adj);
+    vec[2] = (int)on_off;
+    vec[3] = (int)adj_get_value(ui->widget[1]->adj);
+    lv2_atom_forge_set_buffer(&ui->forge, obj_buf, OBJ_BUF_SIZE);
+    LV2_Atom_Forge_Frame frame;
+
+    lv2_atom_forge_frame_time(&ui->forge, 0);
+    LV2_Atom* msg = (LV2_Atom*)lv2_atom_forge_object(&ui->forge, &frame, 1, ui->atom_Int);
+    lv2_atom_forge_property_head(&ui->forge, ui->atom_Vector,0);
+    lv2_atom_forge_vector(&ui->forge, sizeof(int), ui->atom_Int, 4, (void*)vec);
+    lv2_atom_forge_pop(&ui->forge, &frame);
+   
+    ui->write_function(ui->controller, 0, lv2_atom_total_size(msg),
+                       ui->atom_eventTransfer, msg);
+}
+
+void octave_callback(void *w_, void* user_data) {
+    Widget_t *w = (Widget_t*)w_;
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    MidiKeyboard *keys = (MidiKeyboard*)ui->widget[0]->parent_struct;
+    keys->octave = (int)12*adj_get_value(w->adj);
+    expose_widget(ui->widget[0]);
+}
+
+void layout_callback(void *w_, void* user_data) {
+    Widget_t *w = (Widget_t*)w_;
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    MidiKeyboard *keys = (MidiKeyboard*)ui->widget[0]->parent_struct;
+    keys->layout = (int)adj_get_value(w->adj);
+}
+
+void xkey_press(void *w_, void *key_, void *user_data) {
+    Widget_t *w = (Widget_t*)w_;
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    ui->widget[0]->func.key_press_callback(ui->widget[0], key_, user_data);
+}
+
+void xkey_release(void *w_, void *key_, void *user_data) {
+    Widget_t *w = (Widget_t*)w_;
+    X11_UI *ui = (X11_UI*) w->parent_struct;
+    ui->widget[0]->func.key_release_callback(ui->widget[0], key_, user_data);
+}
+
+void get_all_notes_off(Widget_t *w, const int *value) {
+    
+}
+
+void plugin_value_changed(X11_UI *ui, Widget_t *w, PortIndex index) {
+    // do special stuff when needed
+}
+
+void plugin_set_window_size(int *w,int *h,const char * plugin_uri) {
+    (*w) = 616; //set initial width of main window
+    (*h) = 180; //set initial height of main window
+}
+
+const char* plugin_set_name() {
+    return "XKeyboard"; //set plugin name to display on UI
+}
+
+void plugin_create_controller_widgets(X11_UI *ui, const char * plugin_uri) {
+    set_costum_theme(&ui->main);
+    XSelectInput(ui->win->app->dpy, ui->win->widget,StructureNotifyMask|ExposureMask|KeyPressMask 
+                    |EnterWindowMask|LeaveWindowMask|ButtonReleaseMask|KeyReleaseMask
+                    |ButtonPressMask|Button1MotionMask|PointerMotionMask);
+    ui->win->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->win->func.key_press_callback = xkey_press;
+    ui->win->func.key_release_callback = xkey_release;
+
+    ui->widget[0] = create_widget(ui->win->app, ui->win, 0, 80, 616, 100);
+    ui->widget[0]->parent = ui->win;
+    ui->widget[0]->flags &= ~USE_TRANSPARENCY;
+    ui->widget[0]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[0]->scale.gravity = SOUTHEAST;
+    add_midi_keyboard(ui->widget[0], "", 0, 0, 616, 100);
+    MidiKeyboard *keys = (MidiKeyboard*)ui->widget[0]->parent_struct;
+
+    keys->mk_send_note = send_vec;
+    keys->mk_send_all_sound_off = get_all_notes_off;
+    keys->layout = 0;
+    keys->octave = 24;
+    keys->key_size = 21;
+    keys->key_offset = 13;
+    
+    ui->widget[1] = add_lv2_knob(ui->widget[1], ui->win, -1, "Velocity", ui, 540, 0, 50, 70);
+    ui->widget[1]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[1]->parent_struct = ui;
+    set_adjustment(ui->widget[1]->adj, 64.0, 64.0, 0.0, 127.0, 1.0, CL_CONTINUOS);
+    ui->widget[1]->func.key_press_callback = xkey_press;
+    ui->widget[1]->func.key_release_callback = xkey_release;
+
+    add_label(ui->win, "Channel", 460, 5, 60, 20);
+    ui->widget[2] =  add_combobox(ui->win, _("Channel"), 460, 30, 60, 30);
+    ui->widget[2]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[2]->childlist->childs[0]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[2]->scale.gravity = ASPECT;
+    ui->widget[2]->parent_struct = ui;
+    combobox_add_numeric_entrys(ui->widget[2],1,16);
+    combobox_set_active_entry(ui->widget[2], 0);
+    set_adjustment(ui->widget[2]->adj,0.0, 0.0, 0.0, 16.0, 1.0, CL_ENUM);
+    ui->widget[2]->func.key_press_callback = xkey_press;
+    ui->widget[2]->func.key_release_callback = xkey_release;
+
+    add_label(ui->win, "Keymap", 10, 5, 100, 20);
+    ui->widget[3] =  add_combobox(ui->win, _("Mapping"), 10, 30, 100, 30);
+    ui->widget[3]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[3]->childlist->childs[0]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[3]->scale.gravity = ASPECT;
+    ui->widget[3]->parent_struct = ui;
+    combobox_add_entry(ui->widget[3],_("qwertz"));
+    combobox_add_entry(ui->widget[3],_("qwerty"));
+    combobox_add_entry(ui->widget[3],_("azerty (fr)"));
+    combobox_add_entry(ui->widget[3],_("azerty (be)"));
+    combobox_set_active_entry(ui->widget[3], 0);
+    ui->widget[3]->func.value_changed_callback = layout_callback;
+    ui->widget[3]->func.key_press_callback = xkey_press;
+    ui->widget[3]->func.key_release_callback = xkey_release;
+
+    add_label(ui->win, "Octave", 120, 5, 60, 20);
+    ui->widget[4] = add_combobox(ui->win, _("Octave"), 120, 30, 60, 30);
+    ui->widget[4]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[4]->childlist->childs[0]->flags |= NO_AUTOREPEAT | NO_PROPAGATE;
+    ui->widget[4]->scale.gravity = ASPECT;
+    ui->widget[4]->parent_struct = ui;
+    combobox_add_entry(ui->widget[4],_("C 0"));
+    combobox_add_entry(ui->widget[4],_("C 1"));
+    combobox_add_entry(ui->widget[4],_("C 2"));
+    combobox_add_entry(ui->widget[4],_("C 3"));
+    combobox_add_entry(ui->widget[4],_("C 4"));
+    combobox_set_active_entry(ui->widget[4], 2);
+    ui->widget[4]->func.value_changed_callback = octave_callback;
+    ui->widget[4]->func.key_press_callback = xkey_press;
+    ui->widget[4]->func.key_release_callback = xkey_release;
+}
+
+void on_idle(LV2UI_Handle handle) {
+    X11_UI* ui = (X11_UI*)handle;
+    expose_widget(ui->widget[0]);
+}
+
+void plugin_cleanup(X11_UI *ui) {
+    // clean up used sources when needed
+}
+
+int get_u_from_urid(X11_UI *ui, const LV2_URID urid) {
+    if ( urid == ui->note_on) return 1;
+    else if ( urid == ui->note_off) return 2;
+    else return 3;
+}
+
+void plugin_port_event(LV2UI_Handle handle, uint32_t port_index,
+                        uint32_t buffer_size, uint32_t format,
+                        const void * buffer) {
+    X11_UI* ui = (X11_UI*)handle;
+    if (format == ui->atom_eventTransfer) {
+        MidiKeyboard *keys = (MidiKeyboard*)ui->widget[0]->parent_struct;
+        const LV2_Atom* atom = (LV2_Atom*)buffer;
+        if (atom->type == ui->atom_Object) {
+            const LV2_Atom_Object* obj      = (LV2_Atom_Object*)atom;
+            
+            if (obj->body.otype == ui->atom_Int) {
+                const LV2_Atom* vector_data = NULL;
+                const int n_props  = lv2_atom_object_get(obj,ui->atom_Vector, &vector_data, NULL);
+                if (!n_props) return;
+                const LV2_Atom_Vector* vec = (LV2_Atom_Vector*)LV2_ATOM_BODY(vector_data);
+                if (vec->atom.type == ui->atom_Int) {
+                    //int n_elem = (vector_data->size - sizeof(LV2_Atom_Vector_Body)) / vec->atom.size;
+                    int* data;
+                    data = (int*) LV2_ATOM_BODY(&vec->atom);
+                    if ((data[2])) {
+                        set_key_in_matrix(keys->in_key_matrix[data[1]], data[0], true);
+                    } else {
+                        set_key_in_matrix(keys->in_key_matrix[data[1]], data[0], false);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ XKeyboard/XKeyboard.cpp
@@ -0,0 +1,331 @@
+
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+
+#include <lv2.h>
+
+#include <lv2/lv2plug.in/ns/ext/atom/atom.h>
+#include <lv2/lv2plug.in/ns/ext/atom/util.h>
+#include <lv2/lv2plug.in/ns/ext/midi/midi.h>
+#include <lv2/lv2plug.in/ns/ext/urid/urid.h>
+#include "lv2/lv2plug.in/ns/ext/atom/forge.h"
+#include "lv2/lv2plug.in/ns/ext/patch/patch.h"
+
+///////////////////////// MACRO SUPPORT ////////////////////////////////
+
+#define PLUGIN_URI "https://github.com/brummer10/XKeyboard"
+
+#ifndef max
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#endif
+#ifndef min
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+#endif
+
+typedef int PortIndex;
+
+////////////////////////////// PLUG-IN CLASS ///////////////////////////
+
+namespace xkeyboard {
+
+#define XKeyboard__note_on          PLUGIN_URI "#note_on"
+#define XKeyboard__note_off         PLUGIN_URI "#note_off"
+
+class Xxkeyboard
+{
+private:
+    LV2_URID midi_MidiEvent;
+    LV2_URID_Map* map;
+    const LV2_Atom_Sequence* midi_in;
+    LV2_Atom_Sequence* midi_out;
+
+    LV2_URID patch_Set;
+    LV2_URID patch_property;
+    LV2_URID patch_value;
+    LV2_URID note_on;
+    LV2_URID note_off;
+    LV2_URID atom_Int;
+    LV2_URID atom_Vector;
+
+    LV2_Atom                     midiatom; 
+    LV2_Atom_Forge               forge;
+    LV2_Atom_Forge_Frame         frame;
+    uint8_t                      data[3];
+
+    // private functions
+    void write_vector(LV2_URID urid, int value, int channel, int on_off);
+    void send_midi_data(int count, uint8_t controller,
+                             uint8_t note, uint8_t velocity);
+    inline void run_dsp_(uint32_t n_samples);
+    inline void connect_(uint32_t port,void* data);
+    inline void init_dsp_(uint32_t rate);
+    inline void connect_all__ports(uint32_t port, void* data);
+    inline void activate_f();
+    inline void clean_up();
+    inline void deactivate_f();
+public:
+    // LV2 Descriptor
+    static const LV2_Descriptor descriptor;
+    // static wrapper to private functions
+    static void deactivate(LV2_Handle instance);
+    static void cleanup(LV2_Handle instance);
+    static void run(LV2_Handle instance, uint32_t n_samples);
+    static void activate(LV2_Handle instance);
+    static void connect_port(LV2_Handle instance, uint32_t port, void* data);
+    static LV2_Handle instantiate(const LV2_Descriptor* descriptor,
+                                double rate, const char* bundle_path,
+                                const LV2_Feature* const* features);
+    Xxkeyboard();
+    ~Xxkeyboard();
+};
+
+// constructor
+Xxkeyboard::Xxkeyboard() :
+
+    midi_in(NULL),
+    midi_out(NULL) {};
+
+// destructor
+Xxkeyboard::~Xxkeyboard() { };
+
+///////////////////////// PRIVATE CLASS  FUNCTIONS /////////////////////
+
+void Xxkeyboard::init_dsp_(uint32_t rate)
+{
+    // set values for internal ramping
+}
+
+// connect the Ports used by the plug-in class
+void Xxkeyboard::connect_(uint32_t port,void* data)
+{
+    switch ((PortIndex)port)
+    {
+        case 0:
+            midi_in = (const LV2_Atom_Sequence*)data;
+            break;
+        case 1:
+            midi_out = (LV2_Atom_Sequence*)data;
+            break;
+        default:
+            break;
+    }
+}
+
+void Xxkeyboard::activate_f()
+{
+    // allocate the internal DSP mem
+}
+
+void Xxkeyboard::clean_up()
+{
+    // delete the internal DSP mem
+}
+
+void Xxkeyboard::deactivate_f()
+{
+    // delete the internal DSP mem
+}
+
+void Xxkeyboard::send_midi_data(int count, uint8_t controller,
+                             uint8_t note, uint8_t velocity)
+{
+  if(!midi_out) return;
+  data[0] = controller;
+  data[1] = note;
+  data[2] = velocity; 
+  lv2_atom_forge_frame_time(&forge,count);
+  lv2_atom_forge_raw(&forge,&midiatom,sizeof(LV2_Atom));
+  lv2_atom_forge_raw(&forge,data, sizeof(data));
+  lv2_atom_forge_pad(&forge,sizeof(data)+sizeof(LV2_Atom)); 
+}
+
+void Xxkeyboard::write_vector(LV2_URID urid, int value, int channel, int on_off) {
+
+    int vec[3];
+    vec[0] = value;
+    vec[1] = channel;
+    vec[2] = on_off;
+    LV2_Atom_Forge_Frame frame;
+
+    lv2_atom_forge_frame_time(&forge, 0);
+    lv2_atom_forge_object(&forge, &frame, 1, atom_Int);
+    lv2_atom_forge_property_head(&forge, atom_Vector,0);
+    lv2_atom_forge_vector(&forge, sizeof(int), atom_Int, 3, (void*)vec);
+    lv2_atom_forge_pop(&forge, &frame);
+
+}
+
+void Xxkeyboard::run_dsp_(uint32_t n_samples)
+{
+    if(n_samples<1) return;
+
+    lv2_atom_forge_set_buffer(&forge,(uint8_t*)midi_out, midi_out->atom.size);
+    lv2_atom_forge_sequence_head(&forge, &frame, 0);
+    // get controller values
+    LV2_ATOM_SEQUENCE_FOREACH(midi_in, ev) {
+        if (ev->body.type == midi_MidiEvent) {
+            const uint8_t* const msg = (const uint8_t*)(ev + 1);
+            int channel = msg[0]&0x0f;
+            switch (lv2_midi_message_type(msg)) {
+            case LV2_MIDI_MSG_NOTE_ON:
+                send_midi_data(0, msg[0], msg[1], msg[2]);
+                write_vector(note_on, msg[1], channel, 1);
+            break;
+            case LV2_MIDI_MSG_NOTE_OFF:
+                send_midi_data(0, msg[0], msg[1], msg[2]);
+                write_vector(note_off, msg[1], channel, 0);
+            break;
+            case LV2_MIDI_MSG_CONTROLLER:
+                switch (msg[1]) {
+                    case LV2_MIDI_CTL_MSB_MODWHEEL:
+                    case LV2_MIDI_CTL_LSB_MODWHEEL:
+                        //vowel = (float) (msg[2]);
+                    break;
+                    case LV2_MIDI_CTL_ALL_SOUNDS_OFF:
+                    case LV2_MIDI_CTL_ALL_NOTES_OFF:
+                        //
+                    break;
+                    case LV2_MIDI_CTL_RESET_CONTROLLERS:
+                        //pitchbend = 0.0;
+                        //vowel = 0.0;
+                    break;
+                    default:
+                    break;
+                }
+            break;
+            case LV2_MIDI_MSG_BENDER:
+                //pitchbend = ((msg[2] << 7 | msg[1]) - 8192) * 0.00146484375;
+            break;
+            default:
+            break;
+            }
+        } else {
+            const LV2_Atom_Object* obj = (LV2_Atom_Object*)&ev->body;
+            if (obj->body.otype == atom_Int) {
+                const LV2_Atom* vector_data = NULL;
+                const int n_props  = lv2_atom_object_get(obj,atom_Vector, &vector_data, NULL);
+                if (!n_props) return;
+                const LV2_Atom_Vector* vec = (LV2_Atom_Vector*)LV2_ATOM_BODY(vector_data);
+                if (vec->atom.type == atom_Int) {
+                    int n_elem = (vector_data->size - sizeof(LV2_Atom_Vector_Body)) / vec->atom.size;
+                    int* data;
+                    data = (int*) LV2_ATOM_BODY(&vec->atom);
+                    if ((data[2])) send_midi_data(0, 0x90 | data[1], data[0], data[3]);
+                    else send_midi_data(0, 0x80 | data[1], data[0], data[3]);
+                }
+            }
+        }
+    }
+}
+
+void Xxkeyboard::connect_all__ports(uint32_t port, void* data)
+{
+    // connect the Ports used by the plug-in class
+    connect_(port,data); 
+}
+
+////////////////////// STATIC CLASS  FUNCTIONS  ////////////////////////
+
+LV2_Handle 
+Xxkeyboard::instantiate(const LV2_Descriptor* descriptor,
+                            double rate, const char* bundle_path,
+                            const LV2_Feature* const* features)
+{
+    LV2_URID_Map* map = NULL;
+    for (int i = 0; features[i]; ++i) {
+        if (!strcmp(features[i]->URI, LV2_URID__map)) {
+            map = (LV2_URID_Map*)features[i]->data;
+            break;
+        }
+    }
+    if (!map) {
+        return NULL;
+    }
+    // init the plug-in class
+    Xxkeyboard *self = new Xxkeyboard();
+    if (!self) {
+        return NULL;
+    }
+    self->map = map;
+    lv2_atom_forge_init(&self->forge,self->map);
+    self->midi_MidiEvent          = map->map(map->handle, LV2_MIDI__MidiEvent);
+    self->patch_Set               = map->map(map->handle, LV2_PATCH__Set);
+    self->patch_property          = map->map(map->handle, LV2_PATCH__property);
+    self->patch_value             = map->map(map->handle, LV2_PATCH__value);
+    self->note_on                 = map->map(map->handle, XKeyboard__note_on);
+    self->note_off                = map->map(map->handle, XKeyboard__note_off);
+    self->atom_Int                = map->map(map->handle, LV2_ATOM__Int);
+    self->atom_Vector             = map->map(map->handle, LV2_ATOM__Vector);
+    self->midiatom.type = self->midi_MidiEvent;
+    self->midiatom.size = sizeof(self->data);
+    self->init_dsp_((uint32_t)rate);
+    return (LV2_Handle)self;
+}
+
+void Xxkeyboard::connect_port(LV2_Handle instance, 
+                                   uint32_t port, void* data)
+{
+    // connect all ports
+    static_cast<Xxkeyboard*>(instance)->connect_all__ports(port, data);
+}
+
+void Xxkeyboard::activate(LV2_Handle instance)
+{
+    // allocate needed mem
+    static_cast<Xxkeyboard*>(instance)->activate_f();
+}
+
+void Xxkeyboard::run(LV2_Handle instance, uint32_t n_samples)
+{
+    // run dsp
+    static_cast<Xxkeyboard*>(instance)->run_dsp_(n_samples);
+}
+
+void Xxkeyboard::deactivate(LV2_Handle instance)
+{
+    // free allocated mem
+    static_cast<Xxkeyboard*>(instance)->deactivate_f();
+}
+
+void Xxkeyboard::cleanup(LV2_Handle instance)
+{
+    // well, clean up after us
+    Xxkeyboard* self = static_cast<Xxkeyboard*>(instance);
+    self->clean_up();
+    delete self;
+}
+
+const LV2_Descriptor Xxkeyboard::descriptor =
+{
+    PLUGIN_URI ,
+    Xxkeyboard::instantiate,
+    Xxkeyboard::connect_port,
+    Xxkeyboard::activate,
+    Xxkeyboard::run,
+    Xxkeyboard::deactivate,
+    Xxkeyboard::cleanup,
+    NULL
+};
+
+} // end namespace xkeyboard
+
+////////////////////////// LV2 SYMBOL EXPORT ///////////////////////////
+
+extern "C"
+LV2_SYMBOL_EXPORT
+const LV2_Descriptor*
+lv2_descriptor(uint32_t index)
+{
+    switch (index)
+    {
+        case 0:
+            return &xkeyboard::Xxkeyboard::descriptor;
+        default:
+            return NULL;
+    }
+}
+
+///////////////////////////// FIN //////////////////////////////////////
